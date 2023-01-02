4bd31010efe73bc904204a9bc61af206b2240dcd
Mohamed Ben <b.mohamed@tomba.io>
2023-01-02 15:20:18 +0100

    feat: new calls

--- src/Tomba/Services/Finder.php
+++ src/Tomba/Services/Finder.php
@@ -34,7 +34,7 @@ class Finder extends Service
             throw new TombaException('Missing required parameter: "lastName"');
         }
 
-        $path   = str_replace(['{domain}'], [$domain], '/email-finder/{domain}');
+        $path   = str_replace([], [], '/email-finder');
         $params = [];
 
         if (!is_null($firstName)) {
@@ -44,6 +44,90 @@ class Finder extends Service
         if (!is_null($lastName)) {
             $params['last_name'] = $lastName;
         }
+        if (!is_null($domain)) {
+            $params['domain'] = $domain;
+        }
+
+        return $this->client->call(Client::METHOD_GET, $path, [
+            'content-type' => 'application/json',
+        ], $params);
+    }
+
+    /**
+     * Author finder
+     *
+     * This API endpoint generates or retrieves the most likely email address from a blog post url.
+     *
+     * @param string $url
+     * @throws TombaException
+     * @return array
+     */
+    public function authorFinder(string $url): array
+    {
+        if (!isset($url)) {
+            throw new TombaException('Missing required parameter: "url"');
+        }
+
+
+        $path   = str_replace([], [], '/author-finder');
+        $params = [];
+
+        
+        if (!is_null($url)) {
+            $params['url'] = $url;
+        }
+
+        return $this->client->call(Client::METHOD_GET, $path, [
+            'content-type' => 'application/json',
+        ], $params);
+    }
+
+     /**
+     * Linkedin finder
+     *
+     * This API endpoint generates or retrieves the most likely email address from a Linkedin URL.
+     *
+     * @param string $url
+     * @throws TombaException
+     * @return array
+     */
+    public function linkedinFinder(string $url): array
+    {
+        if (!isset($url)) {
+            throw new TombaException('Missing required parameter: "url"');
+        }
+
+
+        $path   = str_replace([], [], '/linkedin');
+        $params = [];
+
+        
+        if (!is_null($url)) {
+            $params['url'] = $url;
+        }
+
+        return $this->client->call(Client::METHOD_GET, $path, [
+            'content-type' => 'application/json',
+        ], $params);
+    }
+
+     /**
+     * Phone Finder
+     *
+     * Search phone are based on the email You give one email and it returns phone data
+     *
+     * @param string $email
+     * @throws TombaException
+     * @return array
+     */
+    public function phoneFinder(string $email): array
+    {
+        if (!isset($email)) {
+            throw new TombaException('Missing required parameter: "email"');
+        }
+
+        $path   = str_replace(['{email}'], [$email], '/phone/{email}');
+        $params = [];
 
         return $this->client->call(Client::METHOD_GET, $path, [
             'content-type' => 'application/json',
